66f590bc8a75f00208b0307fdee8ff9402b2bc8e
Niyad-Sunil-14 <niyadsystem@gmail.com>
2026-05-09 12:33:07 +0530

    DB connected and created file structure

--- backend/DVS/settings.py
+++ backend/DVS/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'DVS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DocVerify_System',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
