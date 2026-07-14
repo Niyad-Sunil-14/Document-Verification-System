4c941eaabe68d23756397fab087d6c25681a3b0e
Niyad-Sunil-14 <niyadsystem@gmail.com>
2026-07-14 21:42:38 +0530

    settings updated

--- backend/DVS/settings.py
+++ backend/DVS/settings.py
@@ -139,8 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# 🚀 FIXED: Tells Django where to collect static assets inside the production container
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
